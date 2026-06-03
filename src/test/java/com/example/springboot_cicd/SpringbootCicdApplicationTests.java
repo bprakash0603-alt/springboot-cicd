97e54f8e26983eea7f6be955b48c2ae947c6e58a
bprakash0603-alt <bprakash0603@gmail.com>
2026-06-03 14:07:55 +0530

    Update SpringbootCicdApplicationTests.java

--- src/test/java/com/example/springboot_cicd/SpringbootCicdApplicationTests.java
+++ src/test/java/com/example/springboot_cicd/SpringbootCicdApplicationTests.java
@@ -1,13 +1,12 @@
 package com.example.springboot_cicd;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootTest
-class SpringbootCicdApplicationTests {
+@SpringBootApplication
+public class SpringbootCicdApplication {
 
-	@Test
-	void contextLoads() {
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCicdApplication.class, args);
+    }
 }
