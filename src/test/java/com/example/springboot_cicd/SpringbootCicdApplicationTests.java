89725d38b7dae27b2a03602a9e3f2246e200303a
bprakash0603-alt <bprakash0603@gmail.com>
2026-06-03 14:09:59 +0530

    Update SpringbootCicdApplicationTests.java

--- src/test/java/com/example/springboot_cicd/SpringbootCicdApplicationTests.java
+++ src/test/java/com/example/springboot_cicd/SpringbootCicdApplicationTests.java
@@ -1,12 +1,13 @@
 package com.example.springboot_cicd;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-public class SpringbootCicdApplication {
+@RestController
+public class HelloController {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringbootCicdApplication.class, args);
+    @GetMapping("/")
+    public String home() {
+        return "Spring Boot CI/CD Project Running Successfully on Port 7080";
     }
 }
